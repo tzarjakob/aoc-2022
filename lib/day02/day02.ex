716ccacbde360ce2b3b500f9bbd0b9afef4775df
tzarjakob <giacomo.mazzucchi@protonmail.com>
2022-12-02 13:24:39 +0100

    day 2 solved

--- lib/day02/day02.ex
+++ lib/day02/day02.ex
@@ -1,11 +1,7 @@
 defmodule Day02 do
   def path() do
-    "./lib/day02/example.aoc"
+    "./lib/day02/input.aoc"
   end
-
-  #   def path() do
-  # "./lib/day02/input.aoc"
-  #   end
 
   def map_values(val) do
     case val do
@@ -50,6 +46,41 @@ defmodule Day02 do
     end)
   end
 
+  def map_values_part_two(val) do
+    case val do
+      "A" -> :rock
+      "B" -> :paper
+      "C" -> :scissors
+      "X" -> :lose
+      "Y" -> :draw
+      "Z" -> :win
+    end
+  end
+
+  def transform_vals(vals) do
+    case vals do
+      [:rock, :win] -> [:rock, :paper]
+      [:rock, :draw] -> [:rock, :rock]
+      [:rock, :lose] -> [:rock, :scissors]
+      [:scissors, :win] -> [:scissors, :rock]
+      [:scissors, :draw] -> [:scissors, :scissors]
+      [:scissors, :lose] -> [:scissors, :paper]
+      [:paper, :win] -> [:paper, :scissors]
+      [:paper, :draw] -> [:paper, :paper]
+      [:paper, :lose] -> [:paper, :rock]
+    end
+  end
+
   def part_two() do
+    File.read!(path())
+    |> String.split("\n")
+    |> Enum.map(fn str ->
+      String.split(str, " ")
+      |> Enum.map(&map_values_part_two/1)
+      |> transform_vals()
+    end)
+    |> Enum.reduce(0, fn x, acc ->
+      acc + el_points(Enum.at(x, 1)) + result_points(Enum.at(x, 0), Enum.at(x, 1))
+    end)
   end
 end
