73cbf3f89dd0c02874d145af580ce58dbb67541b
tzarjakob <giacomo.mazzucchi@protonmail.com>
2022-12-09 10:48:29 +0100

    day 9 solved

--- lib/day09/day09.ex
+++ lib/day09/day09.ex
@@ -26,17 +26,38 @@ defmodule Day09 do
     end
   end
 
+  defp tmovs(hx, hy, tx, ty) do
+    dx = hx - tx
+    dy = hy - ty
+
+    cond do
+      abs(dx) <= 1 and abs(dy) <= 1 -> {tx, ty}
+      # horizontally and vertically
+      dx == 0 and dy == -2 -> {tx, ty - 1}
+      dx == 0 and dy == 2 -> {tx, ty + 1}
+      dx == -2 and dy == 0 -> {tx - 1, ty}
+      dx == 2 and dy == 0 -> {tx + 1, ty}
+      # diagonally
+      dx < 0 and dy > 0 -> {tx - 1, ty + 1}
+      dx > 0 and dy < 0 -> {tx + 1, ty - 1}
+      dx < 0 and dy < 0 -> {tx - 1, ty - 1}
+      dx > 0 and dy > 0 -> {tx + 1, ty + 1}
+      true -> raise("Unhandled case #{hx}-#{hy}, #{tx}-#{ty}")
+    end
+  end
+
   defp move_tail(nhpos, tx, ty) do
     {hx, hy} = nhpos
 
-    cond do
-      abs(hx - tx) <= 1 and abs(hy - ty) <= 1 -> {tx, ty}
-      hx - tx == -2 -> {tx - 1, hy}
-      hx - tx == 2 -> {tx + 1, hy}
-      hy - ty == -2 -> {hx, ty - 1}
-      hy - ty == 2 -> {hx, ty + 1}
-      true -> raise("Unhandled case")
-    end
+    tmovs(hx, hy, tx, ty)
+  end
+
+  defp move_head_tail(hpos, tpos, dir) do
+    {hx, hy} = hpos
+    {tx, ty} = tpos
+    nhpos = move_head(hx, hy, dir)
+    ntpos = move_tail(nhpos, tx, ty)
+    {nhpos, ntpos}
   end
 
   defp steps(_hpos, _tpos, instructions, visited) when instructions == [] do
@@ -45,10 +66,7 @@ defmodule Day09 do
 
   defp steps(hpos, tpos, instructions, visited) do
     {dir, val} = hd(instructions)
-    {hx, hy} = hpos
-    {tx, ty} = tpos
-    nhpos = move_head(hx, hy, dir)
-    ntpos = move_tail(nhpos, tx, ty)
+    {nhpos, ntpos} = move_head_tail(hpos, tpos, dir)
 
     if val - 1 == 0 do
       steps(nhpos, ntpos, tl(instructions), MapSet.put(visited, ntpos))
@@ -62,6 +80,36 @@ defmodule Day09 do
     MapSet.size(steps({0, 0}, {0, 0}, input(), MapSet.new([])))
   end
 
+  defp move_tails(_head, tail, _dir) when tail == [] do
+    []
+  end
+
+  defp move_tails(head, tail, dir) do
+    {tx, ty} = hd(tail)
+    nt = move_tail(head, tx, ty)
+    [nt] ++ move_tails(nt, tl(tail), dir)
+  end
+
+  defp sec_steps(_posl, instructions, visited) when instructions == [] do
+    visited
+  end
+
+  defp sec_steps(posl, instructions, visited) do
+    {dir, val} = hd(instructions)
+
+    {hx, hy} = hd(posl)
+    nh = move_head(hx, hy, dir)
+    nposl = [nh] ++ move_tails(nh, tl(posl), dir)
+
+    if val - 1 == 0 do
+      sec_steps(nposl, tl(instructions), MapSet.put(visited, List.last(nposl)))
+    else
+      new_instr = {dir, val - 1}
+      sec_steps(nposl, [new_instr] ++ tl(instructions), MapSet.put(visited, List.last(nposl)))
+    end
+  end
+
   def part_two() do
+    MapSet.size(sec_steps(List.duplicate({0, 0}, 10), input(), MapSet.new([])))
   end
 end
