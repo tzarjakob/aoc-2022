7d6071066bccbe900dbd1d7c119377c09069cb71
tzarjakob <giacomo.mazzucchi@protonmail.com>
2022-12-10 14:04:29 +0100

    refactor

--- lib/day10/day10.ex
+++ lib/day10/day10.ex
@@ -20,6 +20,48 @@ defmodule Day10 do
     |> process_input()
   end
 
+  def fp_simulation(inslist, cv, cycle, ci, citimer, state, ap) when ap == false do
+    if cycle < 221 and rem(cycle + 20, 40) == 0 do
+      fp_simulation(inslist, cv, cycle, ci, citimer, Map.put(state, cycle, cv), true)
+    else
+      fp_simulation(inslist, cv, cycle, ci, citimer, state, true)
+    end
+  end
+
+  def fp_simulation(inslist, cv, cycle, ci, citimer, state, _ap) when citimer != 0 do
+    fp_simulation(inslist, cv, cycle + 1, ci, citimer - 1, state, false)
+  end
+
+  def fp_simulation(inslist, cv, cycle, ci, _citimer, state, _ap) do
+    ncv =
+      case ci do
+        ["noop"] -> cv
+        ["addx", val] -> cv + String.to_integer(val)
+      end
+
+    nci = hd(inslist)
+
+    ncitimer =
+      case nci do
+        ["noop"] -> 1
+        ["addx", _val] -> 2
+      end
+
+    if tl(inslist) == [] do
+      state
+    else
+      fp_simulation(tl(inslist), ncv, cycle + 1, nci, ncitimer - 1, state, false)
+    end
+  end
+
+  def part_one() do
+    input()
+    |> fp_simulation(1, 0, ["noop"], 0, Map.new(), false)
+    |> Enum.reduce(0, fn {cycle, val}, acc ->
+      acc + cycle * val
+    end)
+  end
+
   def simulation(_inslist, _cv, cycle, _ci, _citimer, state, _ap) when cycle >= 240 do
     state
   end
@@ -30,12 +72,6 @@ defmodule Day10 do
     else
       simulation(inslist, cv, cycle, ci, citimer, state ++ [:dark], true)
     end
-
-    # if cycle < 221 and rem(cycle + 20, 40) == 0 do
-    #   simulation(inslist, cv, cycle, ci, citimer, Map.put(state, cycle, cv), true)
-    # else
-    #   simulation(inslist, cv, cycle, ci, citimer, state, true)
-    # end
   end
 
   def simulation(inslist, cv, cycle, ci, citimer, state, _ap) when citimer != 0 do
@@ -64,24 +100,22 @@ defmodule Day10 do
     end
   end
 
-  def part_one() do
-    res = input()
-    |> simulation(1, 0, ["noop"], 0, [], false)
-    |> Enum.chunk_every(40)
-    |> Enum.map(fn l ->
-      Enum.map(l, fn el ->
-        case el do
-          :light -> "#"
-          :dark -> "."
-        end
-      end) |> Enum.join("")
-    end)
-    |> Enum.join("\n")
+  def part_two() do
+    res =
+      input()
+      |> simulation(1, 0, ["noop"], 0, [], false)
+      |> Enum.chunk_every(40)
+      |> Enum.map(fn l ->
+        Enum.map(l, fn el ->
+          case el do
+            :light -> "#"
+            :dark -> "."
+          end
+        end)
+        |> Enum.join("")
+      end)
+      |> Enum.join("\n")
 
     File.write!("./foo.txt", res)
-
-  end
-
-  def part_two() do
   end
 end
